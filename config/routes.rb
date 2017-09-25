83b2593f042ce20df3c9822b741f6976915f3b7b
横溝 修平 <mash@yokomizo-no-MacBook-Air.local>
2017-09-26 00:29:06 +0900

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  root to: 'home#picture'
+
+  get '*path', controller: 'application', action: 'render_404'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
